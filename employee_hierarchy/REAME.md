7c0c37a30475566f665af40a792ec21bc0c5a489
Achintha Gunasekara <achintha@me.com>
2018-01-24 11:29:17 +1100

    Adding new puzzle

---
+++ employee_hierarchy/REAME.md
@@ -0,0 +1,35 @@
+# Print Employee Hierarchy
+
+Assume there is a REST API available at "http://www.company.corp/api" for accessing employee information The employee information endpoint is "/employee/<id>" Each employee record you retrieve will be a JSON object with the following keys:
+
+  - 'name'  refers to a String that contains the employee's first and last name
+
+  - 'title' refers to a String that contains the employee's job title
+
+  - 'reports' refers to an Array of Strings containing the IDs of the employee's direct reports
+
+Write a function that will take an employee ID and print out the entire hierarchy of employees under that employee.
+
+For example, suppose that Flynn Mackie's employee id is 'A123456789' and his only direct reports are Wesley Thomas and Nina Chiswick. If you provide 'A123456789' as input to your function, you will see the sample output below.
+
+```python
+-----------Begin Sample Output--------------
+Flynn Mackie - Senior VP of Engineering
+  Wesley Thomas - VP of Design
+    Randall Cosmo - Director of Design
+      Brenda Plager - Senior Designer
+  Nina Chiswick - VP of Engineering
+    Tommy Quinn - Director of Engineering
+      Jake Farmer - Frontend Manager
+        Liam Freeman - Junior Software Engineer
+      Sheila Dunbar - Backend Manager
+        Peter Young - Senior Code Cowboy
+-----------End Sample Output--------------
+
+# GET /employee/A123456789
+{
+ "name": "Flynn Mackie",
+ "title": "Senior VP of Engineering",
+ "reports": ["A123456793", "A1234567898"]
+}
+```
---
+++ employee_hierarchy/solution.py
@@ -0,0 +1,70 @@
+""" Print employee hierachy """
+
+import urllib  # pylint: disable=unused-import
+import json  # pylint: disable=unused-import
+
+def get_emp_details(emp_id):
+    """
+    Get employee details from the API
+    Args:
+        emp_id (string): Employee ID
+    Return:
+        Directory: Directory of employee details
+    """
+    #try:
+    #    url == "http://www.linkedin.corp/api//employee/%s" % emp_id
+    #    resonse = urllib.urlopen(url)
+    #    data = json.loads(response.read())
+    #    return data
+    #except Exception ex:
+    #    print "something went wrong - %s" % ex.message
+
+    # just return test data for testing
+    if emp_id == 'A123456791':
+        return {
+            "name": "Flynn Mackie",
+            "title": "Senior VP of Engineering",
+            "reports": ["A123456793", "A1234567898"]
+        }
+    elif emp_id == 'A123456793':
+        return {
+            "name": "Bruce Wayne",
+            "title": "SRE",
+            "reports": []
+        }
+    elif emp_id == 'A1234567898':
+        return {
+            "name": "Pter Parker",
+            "title": "Tester",
+            "reports": []
+        }
+    else:
+        return None
+
+
+def get_emp_hierarchy(emp_id, print_spaces=0):
+    """
+    Print the employee details and get direct reports
+    Args:
+        emp_id (string): Employee ID
+        print_spaces (int): Number of spaces to print before details
+    Returns:
+        None
+    """
+    emp_details = get_emp_details(emp_id=emp_id)
+
+    if not emp_id:
+        return None
+
+    spaces = ''
+
+    for each_space in xrange(print_spaces):  # pylint: disable=unused-variable
+        spaces = "%s%s" % (spaces, ' ')
+
+    print "%s%s - %s" % (spaces, emp_details['name'], emp_details['title'])
+
+    for report in emp_details['reports']:
+        get_emp_hierarchy(emp_id=report, print_spaces=(print_spaces + 2))
+
+
+get_emp_hierarchy(emp_id='A123456791')
