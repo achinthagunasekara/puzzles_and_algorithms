438aaddaa136fb7264be13d5b87d805ce2a1137d
Achintha Gunasekara <achintha@me.com>
2018-03-21 16:49:18 +1100

    Work so far

--- jumping_numbers/solution.py
+++ jumping_numbers/solution.py
@@ -17,15 +17,23 @@ def build_list(number):
 def get_jumping_numbers(number, current_digit=-1, index=0):
     jumping_numbers = []
     integer_list = build_list(number=number)
-    while index < len(integer_list):
+
+#    while index < len(integer_list):
+    while index < 3:
         for each_int in range(10):
             if current_digit - 1 == each_int or current_digit + 1 == each_int:
                 try:
-                    jumping_numbers.append(int("%s%s" % (jumping_numbers[-1], each_int)))
+                    append = int("%s%s" % (jumping_numbers[-1], each_int))
                 except IndexError:
-                    jumping_numbers.append(each_int)
+                    append = each_int
+                print "STUPID NUMBER %s" % append
+                if append <= number:
+                    jumping_numbers.append(append)
+                else:
+                    return jumping_numbers
                 index += 1
                 jumping_numbers = jumping_numbers + get_jumping_numbers(number=number, current_digit=each_int, index=index)
+#    index += 1
     return jumping_numbers
 
 
