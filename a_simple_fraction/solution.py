07d74ebb38d43dea917c95c54c2589015d7175b4
Achintha Gunasekara <achintha@me.com>
2018-06-05 14:15:43 +1000

    Working solution added.

--- a_simple_fraction/solution.py
+++ a_simple_fraction/solution.py
@@ -8,9 +8,11 @@ def get_floating_points(number, divide_by):
         number (int): Number to divide.
         divide_by (int): Number to divide by.
     Returns:
-        str: Result of the division.
+        tuple: Tuple - bool and a float
     """
-    return number / float(divide_by)
+    if (number * 10) % divide_by == number:
+        return True, (number * 10) / divide_by
+    return False, number / float(divide_by)
 
 def divide(number, divide_by):
     """
@@ -25,10 +27,16 @@ def divide(number, divide_by):
     remainder = number % divide_by
     if not remainder:
         return division
-    return get_floating_points(number=remainder, divide_by=divide_by)
+    infinity, val = get_floating_points(number=remainder, divide_by=divide_by)
+    if infinity:
+        return "%s.(%s)" % (division, val)
+    return division + val
 
 NUMBERS = [
     (4, 2),
+    (1, 3),
+    (5, 4),
+    (1, 2),
     (8, 3)
 ]
 
