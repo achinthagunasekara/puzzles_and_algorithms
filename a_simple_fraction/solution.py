5dfa884f32337f6b4239b8d0aa1cb93fba3b49d8
Achintha Gunasekara <achintha@me.com>
2018-06-05 13:02:20 +1000

    Work in progress

--- a_simple_fraction/solution.py
+++ a_simple_fraction/solution.py
@@ -1,4 +1,37 @@
 """ Solution to simple fraction puzzle """
 
+def get_floating_points(number, divide_by):
+
+    """
+    Divide a number by a given number.
+    Args:
+        number (int): Number to divide.
+        divide_by (int): Number to divide by.
+    Returns:
+        str: Result of the division.
+    """
+    return number / float(divide_by)
+
+def divide(number, divide_by):
+    """
+    Divide a number by a given number.
+    Args:
+        number (int): Number to divide.
+        divide_by (int): Number to divide by.
+    Returns:
+        str: Result of the division.
+    """
+    division = number / divide_by
+    remainder = number % divide_by
+    if not remainder:
+        return division
+    return get_floating_points(number=remainder, divide_by=divide_by)
+
+NUMBERS = [
+    (4, 2),
+    (8, 3)
+]
+
 if __name__ == '__main__':
-    pass
+    for each_num in NUMBERS:
+        print divide(number=each_num[0], divide_by=each_num[1])
