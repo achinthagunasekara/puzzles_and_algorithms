7292ab8fe7575b0f4fb633345b05b75da41c74aa
Achintha Gunasekara <achintha@me.com>
2018-08-01 10:21:45 +1000

    Adding a basic solution.

---
+++ return_two_prime_numbers/solution.py
@@ -0,0 +1,15 @@
+"""
+Solution to return two prime numbers puzzle.
+"""
+
+if __name__ == '__main__':
+    INTEGERS = [
+        5,
+        74,
+        1024,
+        66,
+        8,
+        9990
+    ]
+    for integer in INTEGERS:
+        print integer
