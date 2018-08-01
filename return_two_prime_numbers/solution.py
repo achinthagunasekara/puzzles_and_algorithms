f7f3850d828f8fda93c486037c788edda9211220
Achintha Gunasekara <achintha@me.com>
2018-08-01 10:32:08 +1000

    More work

--- return_two_prime_numbers/solution.py
+++ return_two_prime_numbers/solution.py
@@ -4,6 +4,19 @@ Solution to return two prime numbers puzzle.
 
 PRIME_NUMBERS = []
 
+def get_prime_numbers(number):
+    """
+    Given an even number ( greater than 2 ),
+    return two prime numbers whose sum will be equal to given number.
+    There are several combinations possible.
+    This function returns the first such pair.
+    Args:
+        number (int): Number given.
+    Returns:
+        tuple: Tuple with two prime numbers that makes up the given number.
+    """
+    return number
+
 if __name__ == '__main__':
     INTEGERS = [
         5,
@@ -14,4 +27,4 @@ if __name__ == '__main__':
         9990
     ]
     for integer in INTEGERS:
-        print integer
+        print get_prime_numbers(number=integer)
