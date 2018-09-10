308613fe16244dadaf08493408a67a670aacc681
Achintha Gunasekara <achintha@me.com>
2018-09-10 14:42:36 +1000

    More work

--- letter_changes/solution.py
+++ letter_changes/solution.py
@@ -3,5 +3,22 @@
 Solution to letter change puzzle.
 """
 
+
+def change_letters(string):
+    """
+    Change letters in a given string.
+    Args:
+        string (str): String to modify.
+    Returns:
+        str: Modified string.
+    """
+    return string
+
+
+STRINGS = [
+]
+
+
 if __name__ == '__main__':
-    pass
+    for each_string in STRINGS:
+        print change_letters(string=each_string)
