62b6810012248b127eb38a7360250373309c97c7
Achintha Gunasekara <achintha@me.com>
2018-09-10 14:59:33 +1000

    Final working solution.

--- letter_changes/solution.py
+++ letter_changes/solution.py
@@ -4,23 +4,40 @@ Solution to letter change puzzle.
 """
 
 
-def change_letters(string):
+from string import ascii_lowercase
+
+
+def change_letters(input_string):
     """
     Change letters in a given string.
     Args:
-        string (str): String to modify.
+        input_string (str): String to modify.
     Returns:
         str: Modified string.
     """
-    return string
+    modified_string = ""
+    alphabet = list(ascii_lowercase)
+    for each_char in input_string:
+        is_uppercase = each_char.istitle()
+        each_char = each_char.lower()
+        if each_char in alphabet:
+            try:
+                each_char = alphabet[alphabet.index(each_char) + 1]
+            except IndexError:
+                each_char = alphabet[0]
+        if is_uppercase:
+            each_char = each_char.upper()
+        modified_string = "{0}{1}".format(modified_string, each_char)
+    return modified_string
 
 
 STRINGS = [
     'hello*3',
-    'fun times!'
+    'fun times!',
+    'Abcdz'
 ]
 
 
 if __name__ == '__main__':
     for each_string in STRINGS:
-        print "{0} ==> {1}".format(each_string, change_letters(string=each_string))
+        print "{0} ==> {1}".format(each_string, change_letters(input_string=each_string))
