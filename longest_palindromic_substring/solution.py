7867e850e4ac23b7edc1f15846b5335effe7c800
Achintha Gunasekara <achintha@me.com>
2018-09-27 19:08:47 +0800

    working solution added

--- longest_palindromic_substring/solution.py
+++ longest_palindromic_substring/solution.py
@@ -17,14 +17,33 @@ def find_longest_palindromic_substr(string):
     palindromes = []
     logger.info("Provided string is %s", string)
     str_list = list(string)
-    print str_list
+    for index_outter, each_outter_letter in enumerate(str_list):  # pylint: disable=unused-variable
+        temp_list = []
+        sliced_str_list = str_list[index_outter:]
+        for index_inner, each_inner_letter in enumerate(sliced_str_list):
+            temp_list.append(each_inner_letter)
+            num_items_in_tmp = len(temp_list)
+            next_even_list = sliced_str_list[index_inner + 1:index_inner + 1 + num_items_in_tmp]
+            next_even_list.reverse()
+            if temp_list == next_even_list:
+                palindromes.append(
+                    str_list[index_outter:index_outter + index_inner + num_items_in_tmp + 1]
+                )
+            next_odd_list = sliced_str_list[index_inner + 2:index_inner + 2 + num_items_in_tmp]
+            next_odd_list.reverse()
+            if temp_list == next_odd_list:
+                palindromes.append(
+                    str_list[index_outter:index_outter + index_inner + num_items_in_tmp + 2]
+                )
+    return "".join(max(palindromes, key=len))
 
 
 INPUTS = [
     'abwesdabcdeffedcbaaddsd',
     'abbaasua8723bgajag',
     '12345678abba12345678',
-    '00000sssrader343434121'
+    '00000sssrader343434121',
+    '0000sssradar343a4341a21'
 ]
 
 
