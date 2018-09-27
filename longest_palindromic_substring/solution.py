94c4453c187cb15bfe07b5eadf8028b6b66ff225
Achintha Gunasekara <achintha@me.com>
2018-09-27 17:05:00 +0800

    Adding a function to look for a string

--- longest_palindromic_substring/solution.py
+++ longest_palindromic_substring/solution.py
@@ -2,5 +2,26 @@
 Solution to longest palindromic substring puzzle.
 """
 
+import logging
+
+
+def find_longest_palindromic_substr(string):
+    """
+    Find the longest palindromic substring.
+    Args:
+        string (str): String to search for palindromic substrings.
+    Returns:
+        str: Logest palindromic substring in the given string.
+    """
+    logger = logging.getLogger('solution.find_longest_palindromic_substr')
+    logger.info("Provided string is %s", string)
+
+
+INPUTS = [
+    'abwesdabcdeffedcbaaddsd'
+]
+
+
 if __name__ == '__main__':
-    pass
+    for each_input in INPUTS:
+        print find_longest_palindromic_substr(string=each_input)
