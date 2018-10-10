611926013bd7b01e96d768c603aa295e1e774296
Achintha Gunasekara <achintha@me.com>
2018-10-10 13:32:07 +1100

    Adding solution file.

---
+++ count_triplets_with_sum_smaller_than_a_given_value/solution.py
@@ -0,0 +1,12 @@
+"""
+Solution to Count the triplests with sum smaller than a given value.
+"""
+
+INPUTS = [
+    ([-2, 0, 1, 3], 2),
+    ([5, 1, 3, 4, 7], 12)
+]
+
+
+if __name__ == '__main__':
+    pass
