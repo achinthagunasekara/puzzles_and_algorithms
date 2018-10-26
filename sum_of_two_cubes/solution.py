afd6a7647e583432c00207663d8b13f6f785b268
Achintha Gunasekara <achintha@me.com>
2018-10-26 13:44:52 +1100

    Adding more code

--- sum_of_two_cubes/solution.py
+++ sum_of_two_cubes/solution.py
@@ -2,9 +2,14 @@
 Find numbers represented as sum of two cubes for two different pairs - Techie Delight.
 """
 
+
 TEST_CASES = [
     1729,
     4104,
     13832,
     20683
 ]
+
+
+if __nane__ == '__main__':
+    pass
