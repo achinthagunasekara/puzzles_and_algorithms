624333b452dfb652ea8577dc124ee487eae40410
Achintha Gunasekara <achintha@me.com>
2018-10-29 11:37:33 +1100

    Adding more code.

--- balancing_scale/solution.py
+++ balancing_scale/solution.py
@@ -3,6 +3,17 @@ Adding a solution to balancing scale puzzle.
 """
 
 
+def scale_balancing(int_list):
+    """
+    Get what weights needs to be added to balance the scale.
+    Args:
+        int_list (list): List of integers with what's on the scale and available weights.
+    Returns:
+        list: List of weights that must be added to each side.
+    """
+    pass
+
+
 TEST_CASES = [
     [
         [5, 9],
