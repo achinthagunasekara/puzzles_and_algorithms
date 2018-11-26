d917be5d1e927d098d6221ae234577c12bb40621
Achintha Gunasekara <achintha@me.com>
2018-11-26 14:33:15 +1100

    Adding more code

--- draw_a_rocket/solution.py
+++ draw_a_rocket/solution.py
@@ -2,5 +2,25 @@
 Solution to draw a rocket puzzle.
 """
 
-if __name__ == '__main__':
+
+def draw(word):
+    """
+    Draw the rocket with a word.
+    Args:
+        word (str): String to draw on the rocket.
+    Returns:
+        None
+    """
     pass
+
+
+INPUTS = [
+    '',
+    'A',
+    'SPACE'
+]
+
+
+if __name__ == '__main__':
+    for each_word in INPUTS:
+        draw(word=each_word)
