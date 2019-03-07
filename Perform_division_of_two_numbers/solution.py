8594ed61f3ff24acb3a072c319918d86f820023f
Archie Gunasekara <agunasekara@slack-corp.com>
2019-03-07 18:32:22 +1100

    Working thorough solution

--- Perform_division_of_two_numbers/solution.py
+++ Perform_division_of_two_numbers/solution.py
@@ -2,5 +2,22 @@
 Solution - Perform division of two numbers without using division operator (/).
 """
 
+def devide_one(first, second):
+    """
+    Devide 2 numbers without using (/) operator.
+    Args:
+        first (int): First integer.
+        second (int): Second integer.
+    Returns:
+         None
+    """
+    if second == 0:
+        raise ZeroDivisionError("Unable to devide by zero ({0}/{1})".format(first, second))
+
+    sign = bool((first * second) >= 0)
+    first = abs(first)
+    second = abs(second)
+    quotient = 0
+
 if __name__ == '__main__':
     pass
