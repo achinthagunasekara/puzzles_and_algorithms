9fe3ee8efdbb20c1d667b49e479d5cda672df812
Archie Gunasekara <agunasekara@slack-corp.com>
2019-05-10 13:18:11 +1000

    Getting user input

--- find_angle_between_hour_and_minute_hand/solution.py
+++ find_angle_between_hour_and_minute_hand/solution.py
@@ -4,5 +4,24 @@
 Solution to Find Angle Between Hour and Minute Hand Puzzle.
 """
 
+from __future__ import print_function
+import re
+
+
+def run():
+    """
+    Get user input, process and return output.
+    """
+    while True:
+        user_input = raw_input("Please enter the time in hh:mm format: ")
+        print("Is this what you just said? ", user_input)
+        if user_input.to_lower() == 'exit':
+            break
+        pattern = re.compile("^[1]*[1-9]:[0-5][0-9]$")
+        if not pattern.match(user_input):
+            print("Invalud input")
+            continue
+
+
 if __name__ == '__main__':
-    pass
+    run()
