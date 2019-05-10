ab8b1c35692f5d4e9bf6f3bad62690919aa93c7c
Archie Gunasekara <agunasekara@slack-corp.com>
2019-05-10 13:26:53 +1000

    Formatting update

--- find_angle_between_hour_and_minute_hand/solution.py
+++ find_angle_between_hour_and_minute_hand/solution.py
@@ -27,12 +27,15 @@ def run():
     print('Welcome. Please enter time in hh:mm format or exit to end the program')
     while True:
         user_input = raw_input("Please enter the time: ")
+
         if user_input.lower() == 'exit':
             break
+
         pattern = re.compile("^[1]*[1-9]:[0-5][0-9]$")
         if not pattern.match(user_input):
             print("Invalid input. Please try again")
             continue
+
         calculate(user_input)
 
 
