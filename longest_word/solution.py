1750f940ceafc52ef0f2f0866243c4cfa0c430de
Archie Gunasekara <agunasekara@slack-corp.com>
2019-05-24 11:53:51 +1000

    Adding a input taking function

--- longest_word/solution.py
+++ longest_word/solution.py
@@ -4,5 +4,26 @@
 Solution to longest word puzzle.
 """
 
+from __future__ import print_function
+
+
+def run():
+    """
+    Get user input, process and return output.
+    """
+    print('Welcome. Please enter your sentence to find the longest word. Enter `exit` to end.')
+    try:
+        while True:
+            user_input = raw_input("Please enter the time: ")
+
+            if user_input.lower() == 'exit':
+                break
+
+            print(user_input)
+
+    except KeyboardInterrupt:
+        print('\nThanks for using this program. Good bye!')
+
+
 if __name__ == '__main__':
-    pass
+    run()
