dee58cd79e83866ce45ce4779ba36376af16234d
Archie Gunasekara <agunasekara@slack-corp.com>
2019-05-24 12:49:53 +1000

    Working version added.

--- longest_word/solution.py
+++ longest_word/solution.py
@@ -5,6 +5,20 @@ Solution to longest word puzzle.
 """
 
 from __future__ import print_function
+import re
+
+
+def check_for_longest_word(sentence):
+    """
+    Find the longest word in a given sentence.
+    Args:
+        sentence (str): Sentence to find the longest word in.
+    Returns:
+        str: Longest word in the given sentence.
+    """
+    pattern = re.compile("[a-zA-Z]+")
+    result = pattern.findall(sentence)
+    return max(result, key=len) if result else None
 
 
 def run():
@@ -14,12 +28,16 @@ def run():
     print('Welcome. Please enter your sentence to find the longest word. Enter `exit` to end.')
     try:
         while True:
-            user_input = raw_input("Please enter the time: ")
+            user_input = raw_input("Please enter a sentence: ")
 
             if user_input.lower() == 'exit':
                 break
 
-            print(user_input)
+            longest_word = check_for_longest_word(sentence=user_input)
+            if not longest_word:
+                print("No words were found in the input string")
+            else:
+                print(longest_word)
 
     except KeyboardInterrupt:
         print('\nThanks for using this program. Good bye!')
