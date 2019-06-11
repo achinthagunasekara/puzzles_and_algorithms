2fe32368d2f81b8a410ed8e9dca236fee1abc39f
Archie Gunasekara <agunasekara@slack-corp.com>
2019-06-11 10:31:59 +1000

    Getting user input

--- letter_change/solution.py
+++ letter_change/solution.py
@@ -2,5 +2,12 @@
 Solution to letter change puzzle.
 """
 
+def get_input():
+    """
+    Get input from the user.
+    Returns.
+        str: User input string.
+    """
+
 if __name__ == '__main__':
     pass
