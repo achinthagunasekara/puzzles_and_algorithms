e9ae6942045b6924255d189e985da6dc9b92114e
Archie Gunasekara <agunasekara@slack-corp.com>
2019-06-28 11:14:50 +1000

    Adding more code

--- time_delta/solution.py
+++ time_delta/solution.py
@@ -2,5 +2,28 @@
 Solution to time delta puzzle.
 """
 
+def get_test_cases():
+    """
+    Get input from the user.
+    Returns.
+        str: User input string.
+    """
+    test_cases = []
+    while True:
+        number_of_test_cases = raw_input('Please enter number of test cases: ')
+        while number_of_test_cases > 0:
+            test_case = raw_input("Please enter the test case {0}: ".format(number_of_test_cases))
+            test_cases.append(test_case)
+            number_of_test_cases -= 1
+    return test_cases
+
+
+def run():
+    """
+    Run function for the puzzle.
+    """
+    test_cases = get_test_cases()
+
+
 if __name__ == '__main__':
     pass
