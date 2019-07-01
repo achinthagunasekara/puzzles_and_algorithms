14ba3ccd24b7d34bb9e32a48ad6cd29180bed074
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-01 10:46:08 +1000

    More work

--- time_delta/solution.py
+++ time_delta/solution.py
@@ -16,9 +16,9 @@ def get_test_cases():
     number_of_test_cases = input('Please enter number of test cases: ')
     count = 0
     while number_of_test_cases > count:
-        test_case = raw_input("Please enter the test case {0}: ".format(count + 1))
-        test_cases.append(test_case)
         count += 1
+        test_case = raw_input("Please enter the test case {0}: ".format(count))
+        test_cases.append(test_case)
     return test_cases
 
 
