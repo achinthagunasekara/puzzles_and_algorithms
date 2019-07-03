c838486698dea2549f9258188f29f75bdb16b7fd
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-03 13:09:05 +1000

    Improvements

--- time_delta/solution.py
+++ time_delta/solution.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 from datetime import datetime
 
 
-def validate_date_time_input(input_str):
+def get_datetime_input():
     """
     validate input datetime from the user.
     Args:
@@ -15,11 +15,13 @@ def validate_date_time_input(input_str):
     Returns:
         datetime: datetime object.
     """
+    datetime_str = input("Please enter a datetime string: ")
     datetime_object = None
     try:
-        datetime_object = datetime.strptime(input_str, '%a %d %b %Y %H:%M:%S %z')
+        datetime_object = datetime.strptime(datetime_str, '%a %d %b %Y %H:%M:%S %z')
     except ValueError as val_err:
         print("Input error - {0}".format(val_err))
+        get_datetime_input()
     return datetime_object
 
 
@@ -34,19 +36,16 @@ def get_test_cases():
     try:
         number_of_test_cases = int(input('Please enter number of test cases: '))
     except ValueError:
-        print('Please enter a vaild number for number of test cases')
+        print('Error - Invalid number of test cases')
         return None
 
-    inputs_needed = number_of_test_cases * 2
     count = 1
-    while inputs_needed >= count:
-        test_case = input("Please enter the test case {0}: ".format(count))
-        test_case = validate_date_time_input(input_str=test_case)
-        if not test_case:
-            print('Looks like your input is invalid. Please try again')
-        else:
-            test_cases.append(test_case)
-            count += 1
+    while number_of_test_cases >= count:
+        print("Test Case {0}".format(count))
+        test_cases.append(
+            (get_datetime_input(), get_datetime_input())
+        )
+        count += 1
     return test_cases
 
 
@@ -56,7 +55,8 @@ def process(test_cases):
     Args:
         test_cases (list): List of test cases
     """
-    pass
+    for test_case in test_cases:
+        print(test_case)
 
 
 def run():
@@ -65,7 +65,8 @@ def run():
     """
     try:
         test_cases = get_test_cases()
-        print(test_cases)
+        if test_cases:
+            process(test_cases=test_cases)
     except KeyboardInterrupt:
         print('Thank you for using this program. Good bye!')
 
