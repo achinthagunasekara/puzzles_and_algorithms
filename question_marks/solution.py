5be019edd4ebca41c3442131384b4b3485dfb96f
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-12 14:32:28 +1000

    Taking in user input

--- question_marks/solution.py
+++ question_marks/solution.py
@@ -1,6 +1,22 @@
+#!/usr/local/bin/python3
+
 """
 Solution to question marks puzzle.
 """
+
+from __future__ import print_function
+
+def get_input():
+    """
+    Get input from the user.
+    Returns.
+        str: User input string.
+    """
+    while True:
+        user_input = raw_input('Please enter your input: ')
+        if user_input.lower() == 'end':
+            return
+        yield user_input
 
 if __name__ == '__main__':
     pass
