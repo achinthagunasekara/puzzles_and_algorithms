cbe0aecf6a6be9ded82ed12304bb24311118d815
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-19 11:17:04 +1000

    More work to the solution

--- word_order/solution.py
+++ word_order/solution.py
@@ -4,13 +4,20 @@
 Solution to word order puzzle.
 """
 
-
 def get_user_input():
     """
     Get input from the user.
     """
-    pass
+    num_words = input("Enter the number of words you want to input: ")
+    try:
+        num_words = int(num_words)
+    except ValueError:
+        print('Please enter a valid integer')
+        return
+
+    for iteration in range(num_words):
+        num_words = input("Please enter the word {0}: ".format(iteration + 1))
 
 
 if __name__ == '__main__':
-    pass
+    get_user_input()
