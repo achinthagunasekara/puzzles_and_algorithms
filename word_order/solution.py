5ba1d826c17030ad72746ed85a4d98e8e5b8c560
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-19 11:18:27 +1000

    More work done

--- word_order/solution.py
+++ word_order/solution.py
@@ -8,15 +8,19 @@ def get_user_input():
     """
     Get input from the user.
     """
+    words = []
     num_words = input("Enter the number of words you want to input: ")
     try:
         num_words = int(num_words)
     except ValueError:
         print('Please enter a valid integer')
-        return
+        return None
 
     for iteration in range(num_words):
-        num_words = input("Please enter the word {0}: ".format(iteration + 1))
+        words.append(input("Please enter the word {0}: ".format(iteration + 1)))
+
+    return words
+
 
 
 if __name__ == '__main__':
