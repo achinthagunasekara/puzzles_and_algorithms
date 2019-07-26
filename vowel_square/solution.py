78842d4a2c4f7a4afb884cbf315494b2bf95cac1
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-26 11:21:08 +1000

    Entering list of words

--- vowel_square/solution.py
+++ vowel_square/solution.py
@@ -5,6 +5,12 @@ Solution to vowel square puzzle.
 
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
+def get_user_input():
+    """
+    Get input from the user.
+    """
+    words = input('Please enter a comma seperated list of words: ')
+
 
 if __name__ == '__main__':
     pass
