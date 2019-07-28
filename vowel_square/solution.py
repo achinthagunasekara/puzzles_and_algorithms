458900201627ad822990045d9f1e439c4b34f9c8
Archie Gunasekara <agunasekara@slack-corp.com>
2019-07-29 09:13:18 +1000

    Adding more work

--- vowel_square/solution.py
+++ vowel_square/solution.py
@@ -10,7 +10,15 @@ def get_user_input():
     Get input from the user.
     """
     words = input('Please enter a comma seperated list of words: ')
-    return words.replace(' ', '').split(',')
+    words = words.replace(' ', '').split(',')
+
+    iterator = iter(words)
+    the_len = len(next(iterator))
+    if not all(len(word) == the_len for word in words):
+        print('All words must be of same length. Please try again')
+        return get_user_input()
+
+    return words
 
 
 def run():
