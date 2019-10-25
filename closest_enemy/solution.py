e3875d7f7e75a96509c2d07f30455089426c609d
Archie Gunasekara <agunasekara@slack-corp.com>
2019-10-25 15:16:03 +1100

    Good metric collection logic

--- closest_enemy/solution.py
+++ closest_enemy/solution.py
@@ -9,34 +9,44 @@ class ClosestEnemyException(Exception):
     """
     Simple exception for this puzzle.
     """
-    pass
 
-
-def get_input(line):
-    """
-    Get input  from the user.
-    Args:
-        line (int): Line number that's been collected.
-    Returns:
-        List: List of collected values.
-    """
-    inputs = input("Please enter the metric {0}".format(line)).split(" ")
-    for each_input in inputs:
-        if each_input not in ["0", "1", "2"]:
-            raise ClosestEnemyException(
-                "Invalid input {0}. Must be one of 0, 1 or 2".format(each_input)
-            )
-    return inputs
 
 def build_metrix():
     """
-    Get input from the user and build the metrix.
+    Get input from the user and build a metrix.
     Returns:
-        List: 2D matrix with the layout.
+        List: List of lists (metrix).
     """
-    rows = 0
-    cols = 0
-    get_input(0)
+    items_per_line = None
+    metrix = []
+
+    while True:
+        user_input = input("Please enter the metric line {0}: ".format(len(metrix)))
+        if user_input.lower() == 'end':
+            return metrix
+
+        user_input = user_input.split(' ')
+
+        # items_per_line is only set once at the start.
+        if not items_per_line:
+            items_per_line = len(user_input)
+
+        if items_per_line != len(user_input):
+            raise ClosestEnemyException(
+                "Input {0} has less or more than {1} items".format(user_input, items_per_line)
+            )
+
+        for item in user_input:
+            if item not in ["0", "1", "2"]:
+                raise ClosestEnemyException(
+                    "Invalid input {0}. Must be one of 0, 1 or 2".format(item)
+                )
+        metrix.append(user_input)
+    return metrix
+
 
 if __name__ == '__main__':
-    build_metrix()
+    try:
+        build_metrix()
+    except ClosestEnemyException as ex:
+        print("Something went wrong - {0}".format(ex))
