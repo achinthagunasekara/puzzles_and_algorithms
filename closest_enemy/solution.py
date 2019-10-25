6f20f4e8d98cd43188109f98db18a057eb091122
Archie Gunasekara <agunasekara@slack-corp.com>
2019-10-25 14:19:22 +1100

    Getting user input

--- closest_enemy/solution.py
+++ closest_enemy/solution.py
@@ -1,4 +1,16 @@
 #!/usr/local/bin/python3
 
+"""
+Solution to Closest Enemy Puzzle.
+"""
+
+def get_input():
+    """
+    Get input from the user.
+    Returns:
+        List: 2D matrix with the layout.
+    """
+    print("Please enter the metrix")
+
 if __name__ == '__main__':
     pass
