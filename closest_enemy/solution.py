77a3fe89addf652a43e114ca018f4e988ba94b8d
Archie Gunasekara <agunasekara@slack-corp.com>
2019-10-25 15:29:32 +1100

    More validation

--- closest_enemy/solution.py
+++ closest_enemy/solution.py
@@ -18,11 +18,12 @@ def build_metrix():
         List: List of lists (metrix).
     """
     items_per_line = None
+    friendly_found = False
     metrix = []
 
     while True:
         user_input = input("Please enter the metric line {0}: ".format(len(metrix)))
-        if user_input.lower() == 'end':
+        if user_input.lower() == 'end' or user_input == '':
             return metrix
 
         user_input = user_input.split(' ')
@@ -41,6 +42,12 @@ def build_metrix():
                 raise ClosestEnemyException(
                     "Invalid input {0}. Must be one of 0, 1 or 2".format(item)
                 )
+            # In the whole metrix, value 1 is allowed once only.
+            if item == "1":
+                if friendly_found:
+                    raise ClosestEnemyException('You can only enter one friendly (value 1)')
+                friendly_found = True
+
         metrix.append(user_input)
     return metrix
 
@@ -51,6 +58,8 @@ def print_metrix(metrix):
     Args:
         metrix (list): List of lists (metrix).
     """
+    for row in metrix:
+        print(' '.join(row))
 
 def run():
     """
