8cd3e38cd86ce3bcfbc70f2e7ed41f888fa8adf7
Archie Gunasekara <agunasekara@slack-corp.com>
2019-11-11 16:36:46 +1100

    This is a working solution

--- closest_enemy/solution.py
+++ closest_enemy/solution.py
@@ -88,13 +88,22 @@ def find_closest_enemy(metrix):
     if not friendly_pos:
         raise ClosestEnemyException('No friendly found in the metrix')
     print("Friendly is located at {0}".format(friendly_pos))
+
+    closest_enemy = None
+    closest_dist = None
     for col_index, col in enumerate(metrix):
         for row_index, row in enumerate(col):
             if row == '2':
                 enemy_pos = (col_index, row_index)
-                print("Enemy found at {0}".format(enemy_pos))
                 dist = abs(friendly_pos[0] - enemy_pos[0]) + abs(friendly_pos[1] - enemy_pos[1])
-                print("Distance is {0}".format(dist))
+                print("Enemy found at {0} and {1} jumps away".format(enemy_pos, dist))
+                if not closest_enemy:
+                    closest_enemy = enemy_pos
+                    closest_dist = dist
+                if closest_dist > dist:
+                    closest_enemy = enemy_pos
+                    closest_dist = dist
+    return closest_enemy, closest_dist
 
 
 def run():
@@ -104,7 +113,8 @@ def run():
     try:
         metrix = build_metrix()
         print_metrix(metrix=metrix)
-        find_closest_enemy(metrix=metrix)
+        closest_enemy, closest_dist = find_closest_enemy(metrix=metrix)
+        print("Closest enemy is {0} and {1} jumps away".format(closest_enemy, closest_dist))
     except ClosestEnemyException as ex:
         print("Something went wrong - {0}".format(ex))
 
