adc46f4126591cad4e40f363e0d76c1cc47dd9f8
Archie Gunasekara <agunasekara@slack-corp.com>
2020-01-10 09:24:00 +1100

    Match regex

--- chessboard_traveling/solution.py
+++ chessboard_traveling/solution.py
@@ -4,6 +4,8 @@
 Solution.
 """
 
+import re
+
 def get_user_input():
     """
     Get the input from the user.
@@ -11,6 +13,8 @@ def get_user_input():
         string: Location of the chess board space.
     """
     user_input = input('Please enter the location: ')
+    pattern = re.compile("^\([1-8] [1-8]\)\([1-8] [1-8]\)$")
+    print(pattern.match(user_input))
     return user_input
 
 if __name__ == '__main__':
